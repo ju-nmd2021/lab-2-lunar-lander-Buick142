4f20cf0075dffbcdc0a790be8f7f11db730478c7
Noah Buchanan <94912301+Buick142@users.noreply.github.com>
2024-02-21 17:10:03 +0100

    Minor adjustment

--- lunar.js
+++ lunar.js
@@ -55,7 +55,7 @@ function start() {
     text("RULES", width/2, height/2 + 120); 
     textSize(16);  
     text("Press DOWN ARROW to slow your descent.", width/2, height/2 + 140);
-    text("To successfully complete the mission, you must land with the thrusters on.", width/2, height/2 + 160);
+    text("To successfully complete the mission, you must land with the thrusters on.", width/2, height/2 + 156);
 }
 
 const groundLevel = 500;
